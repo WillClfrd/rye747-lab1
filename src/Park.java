cea28e51fadb97bc22a6ee6a86389f6e9ef6741b
William Clifford <w.big.red.dog@gmail.com>
2023-01-29 13:08:08 -0600

    Implemented addDino functionality to Park.

--- src/Park.java
+++ src/Park.java
@@ -2,7 +2,7 @@
 public class Park {
  private String name;
  private int maxCap;
- private Dinosaur Dinosaurs[] = new Dinosaur[maxCap];
+ private Dinosaur dinosaurs[] = new Dinosaur[0];
  
  public Park(String pName, int mCap) {
 	 name = pName;
@@ -12,15 +12,17 @@ public class Park {
  public String toString(Dinosaur dino) {
 	 String parkStr = "Welcome to " + name + "!\n- - - - - - - - - - - - -\n";
 	 int i;
-	 for(i = 0; i < Dinosaurs.length; ++i) {
-		 parkStr += Dinosaurs[i].toString() + "\n";
+	 for(i = 0; i < dinosaurs.length; ++i) {
+		 parkStr += dinosaurs[i].toString() + "\n";
 	 }
 	 return parkStr;
  }
  
  public void addDino(Dinosaur dino) {
-	 if(Dinosaurs.length < maxCap) {
-		 Dinosaurs[Dinosaurs.length] = dino;
+	 if(dinosaurs.length < maxCap) {
+		 Dinosaur dinosaursCopy[] = new Dinosaur[dinosaurs.length + 1];
+		 dinosaursCopy[dinosaurs.length] = dino;
+		 dinosaurs = dinosaursCopy;
 	 }
 	 else {
 		 System.out.println("ERROR: MAX CAPACITY REACHED - Unable to add new dinosaur to " + name);
@@ -28,3 +30,14 @@ public class Park {
  }
  
 }
+//for addDino
+//create array of maxCap size
+	//keep a counter to track how many objects are in the array
+	//use counter to compare to maxCap and to insert new objects
+
+//or
+
+//create array of 0 elements
+	//addDino creates a new array of dinosaurs.length + 1
+		//dino is added to dinosaursCopy[dinosaursCopy.length - 1]
+		//dinosaurs = dinosaursCopy
