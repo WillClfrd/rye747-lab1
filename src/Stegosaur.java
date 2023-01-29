434d2faada1cd7dea6f392738cf435209aafe8a5
William Clifford <w.big.red.dog@gmail.com>
2023-01-29 11:54:36 -0600

    Implemented Dinosaur methods in Theropod, Sauropod, Stegosaur

--- src/Stegosaur.java
+++ src/Stegosaur.java
@@ -3,12 +3,24 @@ public abstract class Stegosaur implements Dinosaur{
 	private String name;
 	private boolean isVeg;
 	
+	public Stegosaur() {
+		
+	}
+	
 	public String toString() {
 		return "Dinosaur";
 	}
 	
+	public String getName() {
+		return "Dino";
+	}
+	
 	public String getType() {
 		return getSubType();
+	}
+	
+	public boolean isVegetarian() {
+		return isVeg;
 	}
 	
 	public abstract String getSubType();
--- src/Theropod.java
+++ src/Theropod.java
@@ -3,12 +3,24 @@ public abstract class Theropod implements Dinosaur{
 	private String name;
 	private boolean isVeg;
 	
+	public Theropod(String name, boolean isVege) {
+		
+	}
+	
 	public String toString() {
 		return "Dinosaur";
 	}
 	
+	public String getName() {
+		return "Dino";
+	}
+	
 	public String getType() {
 		return getSubType();
+	}
+	
+	public boolean isVegetarian() {
+		return isVeg;
 	}
 	
 	public abstract String getSubType();
